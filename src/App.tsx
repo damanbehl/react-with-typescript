b7934a500917637ccb741f734bbc162eb8fd07aa
Daman Behl <damanbehl@gmail.com>
2021-11-29 23:58:16 -0500

    added adding todo capability

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
+import { useState } from "react";
 import Todos from "./components/Todos";
 import Todo from "./models/todo";
+import NewTodo from "./components/NewTodo";
 
 function App() {
-  const todos = [new Todo("Learn React"), new Todo("Learn TypeScript")];
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const addTodoHandler = (enteredText: string) => {
+    setTodos((prevTodos) => {
+      return prevTodos.concat(new Todo(enteredText));
+    });
+  };
+
   return (
     <div>
+      <NewTodo onAddTodo={addTodoHandler} />
       <Todos items={todos} />
     </div>
   );
